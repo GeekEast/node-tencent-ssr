617cccdad8c10807eb970e69bbbdeac92a93f1cc
GeekEast <sx.ba2017@gmail.com>
2019-11-08 23:24:04 +1030

    update

--- 8. 项目1/index.js
+++ 8. 项目1/index.js
@@ -1,1 +1,13 @@
 console.log('hello world')
+
+console.log(Date);
+console.log(Math);
+console.log(setImmediate)
+console.log(setInterval)
+console.log(setTimeout);
+
+console.log(__filename)
+console.log(__dirname)
+
+//  内容很有用
+console.log(process)
